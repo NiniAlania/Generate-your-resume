160336fdf6e41371a9fc9de19ef9ce4bc5714a77
Nini Alania <niniallania@gmail.com>
2023-02-13 14:23:10 +0400

    Add education-info without valid.

--- general-info.js
+++ general-info.js
@@ -1,36 +1,60 @@
-const vector1 = document.querySelector(".vector1");
+const vector = document.querySelectorAll(".vector");
 const pageTitle = document.querySelector(".page-title");
-const button = document.querySelector(".button");
+const nextButtonFirstPg = document.querySelector(".next-button-1st");
 const personalInfo = document.querySelector(".personal-info");
 const workExperience = document.querySelector(".experience");
-const vector2 = document.querySelector(".vector2");
+const education = document.querySelector(".education");
+const nextButtonSecond = document.querySelector(".next-button-2nd");
+const backButtonSecond = document.querySelector(".back-button-2nd");
+const finishButton = document.querySelector(".next-button-3rd");
+const backButtonThird = document.querySelector(".back-button-3rd");
 
 const currentPage = sessionStorage.getItem("page") || 1;
 
 if(currentPage == 1 ) {
   workExperience.style.display = "none";
+  education.style.display = "none";
   personalInfo.style.display ="inline";
 } else if (currentPage == 2 ) {
   personalInfo.style.display = "none";
+  education.style.display = "none";
   workExperience.style.display = "inline";
+} else if( currentPage == 3) {
+  personalInfo.style.display = "none";
+  workExperience.style.display = "none";
+  education.style.display = "inline";
 }
 
-vector1.addEventListener("click", () => {
+vector.forEach(function(vector) {
+  vector.addEventListener("click", () => {
     form.clear();
     location.href= "index.html";
     sessionStorage.removeItem("page");
+  })
 })
 
-button.addEventListener("click", () => {
+nextButtonFirstPg.addEventListener("click", () => {
  personalInfo.style.display = "none";
  workExperience.style.display = "inline";
  sessionStorage.setItem("page", 2);
 })
 
-vector2.addEventListener("click", () => {
+backButtonSecond.addEventListener("click", () => {
   personalInfo.style.display ="inline";
   workExperience.style.display = "none";
   sessionStorage.setItem("page", 1);
+})
+
+nextButtonSecond.addEventListener("click", () => {
+  workExperience.style.display = "none";
+  education.style.display = "inline";
+  sessionStorage.setItem("page", 3);
+})
+
+backButtonThird.addEventListener("click", () => {
+  education.style.display = "none";
+  workExperience.style.display = "inline";
+  sessionStorage.setItem("page", 2);
 })
 
 
